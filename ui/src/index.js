c3b86f7e57946f932c94e1bb421b6c5c68e93ed3
Spyros Tzavikas <apoptygmaberzerk1@gmail.com>
2018-12-17 22:10:52 +0100

    Updated ANT1 stream

--- ui/src/index.js
+++ ui/src/index.js
@@ -32,12 +32,12 @@ export default class StreamingVideoPlayer extends Component {
 }
 
 const options = [
-  { value: 'http://glmxantennatvsec-lh.akamaihd.net/i/live_1@536771/master.m3u8', label: 'ANT1' },
+  { value: 'http://antennatv-lh.akamaihd.net/i/live_1@329667/master.m3u8', label: 'ANT1' },
   { value: 'http://freeview.ashttp9.visionip.tv/live/tvnetwork-hellenictv-nrg-hsslive-25f-4x3-SDh/chunklist.m3u8', label: 'ERT2' },
-  { value: 'http://193.92.37.235:1935/LiveEdgeTV/live@720/master.m3u8', label: 'ALPHA' },
+  { value: 'http://alfakanali-lh.akamaihd.net/i/live_1@90368/master.m3u8', label: 'ALPHA' },
   { value: 'http://klive-a.akamaihd.net/dc-1/live/hls/p/713821/e/1_fp7fyi3j/sd/6000/t/4YED1wipGXOZMnV1TrhLhQ/index-s32.m3u8', label: 'STAR' },
-  { value: 'http://epsilonlivehls.akamaized.net/hls/live/683532/stream1a/res0/playlist_res0.m3u8', label: 'EPSILON' },
-  { value: 'http://anglantennalive5-lh.akamaihd.net/i/live_1@424766/master.m3u8', label: 'MAKEDONIA' },
+  { value: 'http://mgrouphdlive4-f.akamaihd.net/i/live_1@324569/master.m3u8', label: 'EPSILON' },
+  { value: 'http://stream.maktv.gr/i/live_1@332903/master.m3u8', label: 'MAKEDONIA' },
   { value: 'http://master.cystreams.com:25461/live/GalaxyTV/GalaxyTV/184.m3u8', label: 'GALAXYTV' },
 ];
 
